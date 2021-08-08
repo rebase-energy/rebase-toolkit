6059733fb217f0d0a094f30902647bf4496022c4
HenrikKal <henrik.kalvegren@hotmail.com>
2021-08-08 10:59:57 +0200

    Added joblib dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='rebase-toolkit',
     url='https://github.com/rebaseenergy/rebase-sdk',
     packages=find_packages(exclude=["*tests*", "*debug*","*docs*"]),
-    install_requires=['requests>=2.20.0', 'pandas>=1.0.0', 'dill', 'PyYAML', 'dvc', 'mlflow'],
+    install_requires=['requests>=2.20.0', 'pandas>=1.0.0', 'dill', 'PyYAML', 'dvc', 'mlflow', 'joblib'],
     include_package_data=True,
     version='0.0.5-beta',
     license='Apache 2.0',
