ebbc16d3312425a6b30770c52eaffd0844dc9850
HenrikKal <henrik.kalvegren@hotmail.com>
2021-08-08 14:05:28 +0200

    Move main to cli folder

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': [
-            'rebase=main:main'
+            'rebase=rebase.cli.main:main'
         ]
     }
 )
