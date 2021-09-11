45b6419979bad0f91d2151a2d2fb1e6d74733f41
Adam J <adamjac@kth.se>
2021-09-11 14:19:06 +0200

    exchangedtabs with spaces

--- rebase/cli/context.py
+++ rebase/cli/context.py
@@ -3,15 +3,14 @@ import copy
 g_current_context = None
 
 def  current_context():
-	return Context.current()
-
+    return Context.current()
 
 class Stage(object):
-	def __init__(self, name, params=None, *args, **kwargs):
-		self.name = name
-		self.params = params
-		self.dependencies = {}
-		self.outputs = {}
+    def __init__(self, name, params=None, *args, **kwargs):
+        self.name = name
+        self.params = params
+        self.dependencies = {}
+        self.outputs = {}
         self.curr_run = None
 
     def set_current_run(self, run_name):
@@ -23,79 +22,79 @@ class Stage(object):
     def get_run():
         return self.curr_run
 
-	def save_dependencies(self):
-		self.saved_deps = copy.deepcopy(self.dependencies)
+    def save_dependencies(self):
+        self.saved_deps = copy.deepcopy(self.dependencies)
 
-	def clear_dependencies(self):
-		self.dependencies = {}
+    def clear_dependencies(self):
+        self.dependencies = {}
 
-	def clear_outputs(self):
-		self.outputs = {}
+    def clear_outputs(self):
+        self.outputs = {}
 
-	def _add_dep(self, dep_dict, path, name=None, meta=None, **kwargs):
-		dep_dict[name] = { 'name': name or path,
-						   'path': path,
-						   'meta': meta or {},
-						   **kwargs
-						 }
+    def _add_dep(self, dep_dict, path, name=None, meta=None, **kwargs):
+        dep_dict[name] = { 'name': name or path,
+                           'path': path,
+                           'meta': meta or {},
+                           **kwargs
+                         }
 
-	def add_dependency(self, path, name=None, meta=None, externals=None, **kwargs):
-		self._add_dep(self.dependencies, path, name=name, meta=meta, **kwargs)
-		
-	def add_output(self, path, name=None, meta=None, **kwargs):
-		self._add_dep(self.outputs, path, name=name, meta=meta, **kwargs)
+    def add_dependency(self, path, name=None, meta=None, externals=None, **kwargs):
+        self._add_dep(self.dependencies, path, name=name, meta=meta, **kwargs)
+    
+    def add_output(self, path, name=None, meta=None, **kwargs):
+        self._add_dep(self.outputs, path, name=name, meta=meta, **kwargs)
 
-	def get_dependency(self, name):
-		return self.dependencies.get(name, None)
+    def get_dependency(self, name):
+        return self.dependencies.get(name, None)
 
-	def get_dependency_by_path(self, path):
-		deps = [d for d in self.dependencies.values() if d['path'] == path]
-		if len(deps) == 0:
-			return None
-		elif len(deps) > 1:
-			logging.error(f"Dependency with path {path} is registered multiple times")
-			return None
-		else:
-			return deps[0]
+    def get_dependency_by_path(self, path):
+        deps = [d for d in self.dependencies.values() if d['path'] == path]
+        if len(deps) == 0:
+            return None
+        elif len(deps) > 1:
+            logging.error(f"Dependency with path {path} is registered multiple times")
+            return None
+        else:
+            return deps[0]
 
 
 class Context(dict):
 
-	def __init__(self, *args, **kwargs):
-		self.stages = {}
-		self.set_stage("root")		
+    def __init__(self, *args, **kwargs):
+        self.stages = {}
+        self.set_stage("root")        
 
-	def set_stage(self, name, params=None):
-		if name not in self.stages:
-			self.stages[name] = Stage(name, params=params)
-		self._current_stage = self.stages[name]
+    def set_stage(self, name, params=None):
+        if name not in self.stages:
+            self.stages[name] = Stage(name, params=params)
+        self._current_stage = self.stages[name]
 
-	def current_stage(self):
-		return self._current_stage
+    def current_stage(self):
+        return self._current_stage
 
-	def generate_dvc_pipeline(self):
-		dvc = {'stages': {}}
-		for stage_name in sorted(list(self.stages.keys())):
-			if stage_name == "root":
-				continue
+    def generate_dvc_pipeline(self):
+        dvc = {'stages': {}}
+        for stage_name in sorted(list(self.stages.keys())):
+            if stage_name == "root":
+                continue
 
-			stage = self.stages[stage_name]
-			dvc_stage = {
-				"cmd": f"echo \"{stage_name}.py not implemented\"",
-				"deps": sorted([d['path'] for k, d in stage.dependencies.items()]),
-				"outs": sorted([d['path'] for k, d in stage.outputs.items()])}
-			if stage.params:
-				dvc_stage["params"] = sorted(list(stage.params.keys()))
-			dvc['stages'][stage_name] = dvc_stage
-		return dvc
-	
-	@staticmethod
-	def current():
-		global g_current_context
+            stage = self.stages[stage_name]
+            dvc_stage = {
+                "cmd": f"echo \"{stage_name}.py not implemented\"",
+                "deps": sorted([d['path'] for k, d in stage.dependencies.items()]),
+                "outs": sorted([d['path'] for k, d in stage.outputs.items()])}
+            if stage.params:
+                dvc_stage["params"] = sorted(list(stage.params.keys()))
+            dvc['stages'][stage_name] = dvc_stage
+        return dvc
+    
+    @staticmethod
+    def current():
+        global g_current_context
 
-		if g_current_context is None:
-			g_current_context = Context()
+        if g_current_context is None:
+            g_current_context = Context()
 
-		return g_current_context
+        return g_current_context
 
 __all__ = ['Context', 'current']
