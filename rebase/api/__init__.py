8a6c1c029521daf56d61cf08329f8a6d42c4f89f
HenrikKal <henrik.kalvegren@hotmail.com>
2021-09-28 09:31:45 +0200

    Remove temp site2

--- rebase/api/__init__.py
+++ rebase/api/__init__.py
@@ -1,6 +1,5 @@
 from .predicter import Predicter, Model
 from .site import Site, SiteTemplate
-from .site2 import Site2
 from .weather import *
 from .backend import *
 from .runner import ModelRunner
